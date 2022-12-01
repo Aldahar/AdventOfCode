00960428f1df63926e0393a7e9821070e9f5f0fb
Aldahar <aldahar@gmx.de>
2022-12-01 01:54:32 +0100

    Setup + Solution for first day of 2021

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode.Year2021;
+var input = "";
+List<int> depths = new List<int>();
+
+while (input != "9548")
+{
+    input = Console.ReadLine();
+    depths.Add(Int32.Parse(input));
+} 
+
+var  depthCounter = FirstDayOfChristmas.BetterSonar(depths);
+Console.WriteLine(depthCounter);
---
+++ Year2021/FirstDayOfChristmas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2021
+{
+    public class FirstDayOfChristmas
+    {
+
+        public static int Sonar(List<int> depths)
+        {
+            var deeperCount = 0;
+            for (int i = 1; i < depths.Count; i++)
+            {
+                if (depths[i] > depths[i - 1])
+                {
+                    deeperCount++;
+                }
+            }
+            return deeperCount;
+        }
+
+        public static int BetterSonar(List<int> depths)
+        {
+            int deeperCount = 0;
+            var prevWindow = int.MaxValue;
+            var currentWindow = 0 ;
+            for (int i = 1; i < depths.Count-1; i++)
+            {
+                currentWindow = depths[i - 1] + depths[i]+depths[i+1];
+                if (currentWindow > prevWindow)
+                {
+                    deeperCount++;
+                }
+                prevWindow = currentWindow;
+            }
+            return deeperCount;
+        }
+    }
+}
